5ce8ab68f50ffd44b1bbaab0fa7cfec1e02838e2
Suricato00 <94184925+Suricato00@users.noreply.github.com>
2021-12-06 18:07:51 +0100

    Create CrabHub Documentation.lua

---
+++ CrabHub Documentation.lua
@@ -0,0 +1,18 @@
+--[[
+ALL OF THE COMMANDS MUST HAVE /e BEFORE TO WORK
+just like you would do an emote
+
+example: "/e rejoin"
+
+all of them have two variants, the long one and the short one
+example: there is "rejoin" or "rj" for short
+
+--Commands--
+"AntiAfk" or "Afk" (you can go away from the computer without getting kicked)
+"Rejoin" or "Rj" (Rejoins you to the same game)
+"FpsBoost" or "Fps" (makes the game look super bad but its good for afk farming or if you have a trash pc)
+"WalkSpeed" or "Ws" (changes your speed to the designed amount)
+
+
+
+]]
